6d1c9947c582d63fbf7a119e1868be183c3367a3
Hrach Brutyan <hrachbrutyan481@gmail.com>
2022-09-03 14:03:36 +0400

    Add Task API

---
+++ script6.js
@@ -0,0 +1,21 @@
+
+// 5. Pending API
+
+// let intervalID
+
+// async function promiseResponse () {
+//     let a = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let b = await a.json();
+    
+//     if(a.status !== 200) {
+//         intervalID = setTimeout(() => {
+//             console.log(b)
+//         },10000)
+//     }else {
+//         console.log(b)
+//     }
+// }
+ 
+// promiseResponse()
+
+
